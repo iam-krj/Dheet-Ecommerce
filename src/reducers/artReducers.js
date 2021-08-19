e028f005a9428bc2780fcc73bd1f56a6c8184cf8
Sharique <mdsharique1205@gmail.com>
2021-08-19 15:10:22 +0530

    Renamed artReducers to artReducer

--- src/reducers/artReducers.js
+++
@@ -1,9 +0,0 @@
-const artReducer = (state = "", action) => {
-    switch (action.type) {
-        case "UPDATE_ART":
-            return action.payload;
-        default:
-            return state;
-    };
-};
-export default artReducer;
